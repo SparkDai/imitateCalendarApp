3c4a81ff3916d2c701229f752e4a0e1016c3f86a
hanle <826393760@qq.com>
2021-01-25 17:56:25 +0800

    Initial commit

---
+++ ImitateCalendarApp/ImitateCalendarApp/ImitateCalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImitateCalendarAppApp.swift
+//  ImitateCalendarApp
+//
+//  Created by daishenao on 2021/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ImitateCalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
